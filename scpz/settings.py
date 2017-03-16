250cafa38b25e1a717b0be36a48df1f108034ca9
Oladayo Osikoya <alwaysdeone@gmail.com>
2017-03-16 10:01:56 +0000

    Set up static files. Display homepage template.

--- scpz/settings.py
+++ scpz/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['scpz-deone.c9users.io']
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'scpz.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# My Settings
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
